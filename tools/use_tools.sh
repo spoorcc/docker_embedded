8cf67ae836520c03b7c0ce86d9b47c0b94ad9b9b
ben <ben.spoor@gmail.com>
2017-10-23 20:32:08 +0200

    Move build knowledge to build pipeline

--- tools/use_tools.sh
+++ tools/use_tools.sh
@@ -13,7 +13,9 @@ projectroot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 artifactsdir=$(mktemp -d "${TMPDIR:-/tmp/}artifacts.XXXXXXXXXXXX")
 
 banner "Build artifacts using MSP430 Cmake docker image"
-docker run --rm -it -v ${projectroot}/src:/src:ro -v ${artifactsdir}:/artifacts cmake_msp
+docker run --rm -it -v ${projectroot}/src:/src:ro \
+                    -v ${artifactsdir}:/artifacts \
+                    cmake_msp /src/build.sh
 
 number_of_files=$(find ${artifactsdir} -type f | wc -l)
 banner "${number_of_files} build artifact(s) in ${artifactsdir}"
