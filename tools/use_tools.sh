a60cdde00e4e860f2994e1165215c59e1a831122
ben <ben.spoor@gmail.com>
2017-10-23 21:33:46 +0200

    Make pipeline script more generic

--- tools/use_tools.sh
+++ tools/use_tools.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+projectroot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+
 function banner() {
     sep="---------------------------------------------------------------------"
     echo $sep
@@ -8,18 +10,28 @@ function banner() {
     echo $sep
 }
 
-projectroot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+function build() {
+    docker_image="$1"
+    srcdir="$2"
+    artifactsdir=$3
+    build_script="$4"
+
+    banner "Build artifacts using ${docker_image} docker image"
+    docker run --rm -it -v ${srcdir}:/src:ro \
+                        -v ${artifactsdir}:/artifacts \
+                        ${docker_image} ${build_script}
+
+    number_of_files=$(find ${artifactsdir} -type f | wc -l)
+    banner "${number_of_files} build artifact(s) in ${artifactsdir}"
+    ls ${artifactsdir}
+    banner "Done"
+}
 
 artifactsdir=$(mktemp -d "${TMPDIR:-/tmp/}artifacts.XXXXXXXXXXXX")
 
-banner "Build artifacts using MSP430 Cmake docker image"
-docker run --rm -it -v ${projectroot}/src:/src:ro \
-                    -v ${artifactsdir}:/artifacts \
-                    cmake-msp430 /src/build.sh
 
-number_of_files=$(find ${artifactsdir} -type f | wc -l)
-banner "${number_of_files} build artifact(s) in ${artifactsdir}"
-ls ${artifactsdir}
-banner "Done"
+build cmake-msp430 "${projectroot}/src" "${artifactsdir}" "/src/build.sh"
+
+
 
 
