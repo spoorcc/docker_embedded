f2a54431860ea475dbb19ef03de6be30357c2341
ben <ben.spoor@gmail.com>
2017-10-26 23:25:50 +0200

    Update local_ci to use cmake

--- tools/local_ci.sh
+++ tools/local_ci.sh
@@ -4,7 +4,7 @@ function local_ci() {
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 echo "$basedir"
 while inotifywait -r --exclude='.*(\/bld\/.*|.sw(p|o))' -e modify $basedir/src; do
-    $basedir/tools/use_tools.sh
+    cmake --build $basedir/bld
     # Wait a little bit before rescheduling
     sleep 3
 done
