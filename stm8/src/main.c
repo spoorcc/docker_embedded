35a11c6bd31a5b1a255d8481cdcfc390a5f34bc4
ben <ben.spoor@gmail.com>
2017-11-06 23:17:57 +0100

    Optimized code

--- stm8/src/main.c
+++ stm8/src/main.c
@@ -1,37 +1,37 @@
 #include <stdint.h>
 #include "stm8s208s.h"
 
-unsigned int clock(void)
-{
-    unsigned char h = TIM1_PCNTRH;
-    unsigned char l = TIM1_PCNTRL;
-    return((unsigned int)(h) << 8 | l);
-}
-
 void main(void)
 {
-    CLK_DIVR = 0x00; // Set the frequency to 16 MHz
-    CLK_PCKENR2 |= 0x02; // Enable clock to timer
+    CLK_CKDIVR = 0x00; // Set the frequency to 16 MHz
+    //CLK_PCKENR1 |= PCKEN17; // Enable clock to timer
+    __asm__ ("	bset	0x50c7, #7");
 
     // Configure timer
     // 1000 ticks per second
-    TIM1_PSCRH = 0x3e;
-    TIM1_PSCRL = 0x80;
+    TIM1_PSCRH = (16000>>8);
+    TIM1_PSCRL = (uint8_t)(16000 & 0xff);
     // Enable timer
     TIM1_CR1 = 0x01;
 
     /* Set PB0 as output */
-    PB_DDR |= (GPIO_OUTPUT_MODE<<PIN0);
-
-    /* Set Push/Pull mode */
-    PB_CR1 |= (0x01<<PIN0);
+    //PB_DDR |= (GPIO_OUTPUT_MODE<<PIN0);
+    __asm__ ("	bset	0x5007, #0");
 
     /* Set low speed mode */
-    PB_CR2 |= (0x00<<PIN0);
+    //PB_CR2 &= ~(0x01<<PIN0);
+    __asm__ ("	bres	0x5009, #0");
+
+    /* Set Push/Pull mode */
+    //PB_CR1 |= (0x01<<PIN0);
+    __asm__ ("	bset	0x5008, #0");
+
 
     for(;;)
     {
-        if (clock() % 1000 <= 500)
-            PB_ODR ^= (0x01<<PIN0);
+        if (((unsigned char)TIM1_CNTRL) % 1000 <= 500) {
+            //PB_ODR ^= (0x01<<PIN0);
+            __asm__ ("	bcpl	0x5005, #0");
+        }
     }
 }
