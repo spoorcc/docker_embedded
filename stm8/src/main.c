6fecae9ae047c729e6971bf1079066b986866c96
ben <ben.spoor@gmail.com>
2017-11-08 22:16:32 +0100

    Use more clear macros

--- stm8/src/main.c
+++ stm8/src/main.c
@@ -3,35 +3,29 @@
 
 void main(void)
 {
-    CLK_CKDIVR = 0x00; // Set the frequency to 16 MHz
-    //CLK_PCKENR1 |= PCKEN17; // Enable clock to timer
-    __asm__ ("	bset	0x50c7, #7");
+    MEMLOC(CLK_CKDIVR) = 0x00; // Set the frequency to 16 MHz
+    BITSET(CLK_PCKENR1, 7);
 
     // Configure timer
     // 1000 ticks per second
-    TIM1_PSCRH = (16000>>8);
-    TIM1_PSCRL = (uint8_t)(16000 & 0xff);
+    MEMLOC(TIM1_PSCRH) = (16000>>8);
+    MEMLOC(TIM1_PSCRL) = (uint8_t)(16000 & 0xff);
     // Enable timer
-    TIM1_CR1 = 0x01;
+    MEMLOC(TIM1_CR1) = 0x01;
 
     /* Set PB0 as output */
-    //PB_DDR |= (GPIO_OUTPUT_MODE<<PIN0);
-    __asm__ ("	bset	0x5007, #0");
+    BITSET(PB_DDR, 0);
 
     /* Set low speed mode */
-    //PB_CR2 &= ~(0x01<<PIN0);
-    __asm__ ("	bres	0x5009, #0");
+    BITRST(PB_CR2, 0);
 
     /* Set Push/Pull mode */
-    //PB_CR1 |= (0x01<<PIN0);
-    __asm__ ("	bset	0x5008, #0");
-
+    BITSET(PB_CR1, 0);
 
     for(;;)
     {
-        if (((unsigned char)TIM1_CNTRL) % 1000 <= 500) {
-            //PB_ODR ^= (0x01<<PIN0);
-            __asm__ ("	bcpl	0x5005, #0");
+        if ((MEMLOC(TIM1_CNTRL)) % 1000 <= 500) {
+            BITTOG(PB_ODR, 0);
         }
     }
 }
--- stm8/src/stm8s208s.h
+++ stm8/src/stm8s208s.h
@@ -2,32 +2,27 @@
 
 #include <stdint.h>
 
-#define PB_ODR   (*(volatile uint8_t *)0x5005)
-#define PB_IDR   (*(volatile uint8_t *)0x5006)
-#define PB_DDR   (*(volatile uint8_t *)0x5007)
-#define PB_CR1   (*(volatile uint8_t *)0x5008)
-#define PB_CR2   (*(volatile uint8_t *)0x5009)
+#define STR(x)   #x
+#define XSTR(x)  STR(x)
+#define BITSET(addr, bit) __asm__("	bset	"XSTR(addr)", #"XSTR(bit));
+#define BITRST(addr, bit) __asm__("	bres	"XSTR(addr)", #"XSTR(bit));
+#define BITTOG(addr, bit) __asm__("	bcpl	"XSTR(addr)", #"XSTR(bit));
+#define MEMLOC(addr) (*(volatile uint8_t *)addr)
 
-#define CLK_CKDIVR   (*(volatile uint8_t *)0x50c6)
-#define CLK_PCKENR1   (*(volatile uint8_t *)0x50c7)
+#define PB_ODR   0x5005
+#define PB_IDR   0x5006
+#define PB_DDR   0x5007
+#define PB_CR1   0x5008
+#define PB_CR2   0x5009
+
+#define CLK_CKDIVR   0x50c6
+#define CLK_PCKENR1   0x50c7
 #define PCKEN17 (0x01<<7) /* Enables clock to TIM1 */
-#define CLK_PCKENR2   (*(volatile uint8_t *)0x50ca)
+#define CLK_PCKENR2   0x50ca
 
+#define TIM1_CR1    0x5250
+#define TIM1_CNTRH  0x52bf
+#define TIM1_CNTRL  0x52c0
+#define TIM1_PSCRH  0x5260
+#define TIM1_PSCRL  0x5261
 
-#define TIM1_CR1   (*(volatile uint8_t *)0x5250)
-#define TIM1_CNTRH   (*(volatile uint8_t *)0x52bf)
-#define TIM1_CNTRL   (*(volatile uint8_t *)0x52c0)
-#define TIM1_PSCRH   (*(volatile uint8_t *)0x5260)
-#define TIM1_PSCRL   (*(volatile uint8_t *)0x5261)
-
-#define PIN0 (0)
-#define PIN1 (1)
-#define PIN2 (2)
-#define PIN3 (3)
-#define PIN4 (4)
-#define PIN5 (5)
-#define PIN6 (6)
-#define PIN7 (7)
-
-#define GPIO_INPUT_MODE (0x00)
-#define GPIO_OUTPUT_MODE (0x01)
