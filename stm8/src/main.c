f35eea985e4b20020c8a91533fcd769c5745662b
ben <ben.spoor@gmail.com>
2017-11-08 22:32:27 +0100

    Reduce ticks to 250 to use 8bit diff

--- stm8/src/main.c
+++ stm8/src/main.c
@@ -3,28 +3,22 @@
 
 void main(void)
 {
-    MEMLOC(CLK_CKDIVR) = 0x00; // Set the frequency to 16 MHz
-    BITSET(CLK_PCKENR1, 7);
+    MEMLOC(CLK_CKDIVR) = 0x00; /* Set the frequency to 16 MHz */
+    BITSET(CLK_PCKENR1, 7); /* Enable clk to TIM1 */
 
     // Configure timer
-    // 1000 ticks per second
-    MEMLOC(TIM1_PSCRH) = (16000>>8);
-    MEMLOC(TIM1_PSCRL) = (uint8_t)(16000 & 0xff);
-    // Enable timer
-    MEMLOC(TIM1_CR1) = 0x01;
+    // 250 ticks per second
+    MEMLOC(TIM1_PSCRH) = (64000>>8);
+    MEMLOC(TIM1_PSCRL) = (uint8_t)(64000 & 0xff);
 
-    /* Set PB0 as output */
-    BITSET(PB_DDR, 0);
+    MEMLOC(TIM1_CR1) = 0x01; /* Enable timer */
 
-    /* Set low speed mode */
-    BITRST(PB_CR2, 0);
+    BITSET(PB_DDR, 0); /* Set PB0 as output */
+    BITRST(PB_CR2, 0); /* Set low speed mode */
+    BITSET(PB_CR1, 0); /* Set Push/Pull mode */
 
-    /* Set Push/Pull mode */
-    BITSET(PB_CR1, 0);
-
-    for(;;)
-    {
-        if ((MEMLOC(TIM1_CNTRL)) % 1000 <= 500) {
+    for(;;) {
+        if ((MEMLOC(TIM1_CNTRL)) % 250 <= 125) {
             BITTOG(PB_ODR, 0);
         }
     }
